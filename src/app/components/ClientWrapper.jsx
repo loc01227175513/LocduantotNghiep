077d665511de5922c90d07bb0480ec00a58628a9
loc01227175513 <dndvelogs@gmail.com>
2024-12-11 11:33:09 +0700

    sssss
    sssss

--- src/app/components/ClientWrapper.jsx
+++ src/app/components/ClientWrapper.jsx
@@ -1,51 +1,51 @@
-'use client'
+  'use client'
 
-import { createContext, useContext, useState } from 'react'
-import { ToastContainer } from 'react-toastify'
+  import { createContext, useContext, useState } from 'react'
+  import { ToastContainer } from 'react-toastify'
 
-// Create a context for toggle state
-export const ToggleContext = createContext({
-  isToggled: false,
-  setToggle: () => {},
-})
+  // Create a context for toggle state
+  export const ToggleContext = createContext({
+    isToggled: false,
+    setToggle: () => {},
+  })
 
-export default function ClientWrapper({ children }) {
-  const [isToggled, setIsToggled] = useState(false)
+  export default function ClientWrapper({ children }) {
+    const [isToggled, setIsToggled] = useState(false)
 
-  const toggleValue = {
-    isToggled,
-    setToggle: setIsToggled
+    const toggleValue = {
+      isToggled,
+      setToggle: setIsToggled
+    }
+
+    return (
+      <ToggleContext.Provider value={toggleValue}>
+        <div>
+          {children}
+        </div>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable={false}
+          pauseOnHover={false}
+          theme="light"
+          limit={3}
+          enableMultiContainer={false}
+          containerId="default"
+        />
+      </ToggleContext.Provider>
+    )
   }
 
-  return (
-    <ToggleContext.Provider value={toggleValue}>
-      <div>
-        {children}
-      </div>
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable={false}
-        pauseOnHover={false}
-        theme="light"
-        limit={3}
-        enableMultiContainer={false}
-        containerId="default"
-      />
-    </ToggleContext.Provider>
-  )
-}
-
-// Custom hook to use toggle
-export const useToggle = () => {
-  const context = useContext(ToggleContext)
-  if (context === undefined) {
-    throw new Error('useToggle must be used within a ToggleProvider')
-  }
-  return context
-} 
+  // Custom hook to use toggle
+  export const useToggle = () => {
+    const context = useContext(ToggleContext)
+    if (context === undefined) {
+      throw new Error('useToggle must be used within a ToggleProvider')
+    }
+    return context
+  } 
